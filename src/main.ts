67a20ff25f54132cbb282f174ecc52443d15e5b6
grbvtsk <ngraboveckij@gmail.com>
2025-05-26 19:10:45 +0300

    added cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://client-train.vercel.app'],
     credentials: true,
   });
 
